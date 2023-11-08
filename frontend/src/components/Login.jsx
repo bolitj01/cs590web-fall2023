4cd3c4db7b2d73f50d0e55fe666b43623948e2b3
hrithikmani <hrithik.mani2000@gmail.com>
2023-11-08 01:40:36 -0500

    feat: profile frontend

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -44,7 +44,7 @@ const [ercond,setErcond] = useState(false);
   <header className="header">
     <img id="logo" src="mainlogo.png" />
     <nav className="navbar">
-    <a href="/"> <span>Home</span></a>
+        <a href="homepage.html">Home</a>
         <Link to="/signup"><span>Sign Up</span></Link>
 
     </nav>
--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 function Posts() {
   return (
-    <div>
-     Posts works 
-    </div>
+    <>
+    </>
   )
 }
 
